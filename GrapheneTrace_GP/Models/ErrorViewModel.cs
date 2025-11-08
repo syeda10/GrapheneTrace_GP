2766ba43af3390cda3e6662c8138afc09569d7ba
syeda <syeda@LAPTOP-IU6DM3I1>
2025-11-08 19:03:19 +0000

    Add project files.

---
+++ GrapheneTrace_GP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GrapheneTrace_GP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
