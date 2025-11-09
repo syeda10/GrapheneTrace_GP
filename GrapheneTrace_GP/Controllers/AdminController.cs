5490d1a0b326bef15bdcf9a76c989c39a9da92df
syeda10 <syedalaiba276@gmail.com>
2025-11-09 21:04:47 +0000

    Added Admin controller

---
+++ GrapheneTrace_GP/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GrapheneTrace_GP.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        } //laiba's comment
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
