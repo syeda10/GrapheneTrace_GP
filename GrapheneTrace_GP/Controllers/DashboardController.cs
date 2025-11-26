73bf3f647afcc4f7fb6c32fdd1df6700b81677f9
syeda10 <syedalaiba276@gmail.com>
2025-11-26 20:23:37 +0000

    Dashboard is all set for Admin, with different sidebar,topbar and css

---
+++ GrapheneTrace_GP/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GrapheneTrace_GP.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.UserEmail = "username@gmail.com"; // later: set from auth
+            return View();
+        }
+    }
+}
